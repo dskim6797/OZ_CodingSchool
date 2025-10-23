7281ea56492fed1a5dfe9984d6dd536856e8d1f7
dskim6797 <dskim6797@gmail.com>
2025-10-24 08:57:59 +0900

    style: Remove unused blank lines

--- homeworks/Python/DAY8/challenges/banking_system/utils/decorators.py
+++ homeworks/Python/DAY8/challenges/banking_system/utils/decorators.py
@@ -5,11 +5,9 @@ def validate_transaction(func: Callable) -> Callable:
     def wrapper(self, amount: int) -> None:
         if amount <= 0:
             raise NegativeAmountError()
-        
         if func.__name__ == 'withdraw':
             current_balance = self.get_balance() 
             if amount > current_balance:
                 raise InsufficientFundsError(current_balance)
-        
         return func(self, amount)
     return wrapper
