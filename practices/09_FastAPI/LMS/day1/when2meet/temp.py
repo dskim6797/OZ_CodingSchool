63c9c056cc928352d4e2a28d1a7f252f8d27be03
dskim6797 <dskim6797@gmail.com>
2025-12-04 18:04:05 +0900

    FastAPI - day1 LMS practices

--- practices/09_FastAPI/LMS/day1/when2meet/temp.py
+++ practices/09_FastAPI/LMS/day1/when2meet/temp.py
@@ -6,3 +6,13 @@ import temp2 # noqa (특정 라인에 대해서 lint를 하고 싶지 않을 때
 
 
 # O = "abc"
+
+a = 123
+# reveal_type(a) # mypy용 type print
+
+my_int: int = 1
+my_list: list[str] = ["abc", "def"]
+my_tuple: tuple[str, str] = ("abc", "def")
+my_tuple2: tuple[str, ...] = ("abc", "def") # 길이를 모르는 경우
+my_dict: dict[str, int] = {"a": 1, "b": 2, "c": 3, "d": 4}
+or_type_list: list[str | int] = ["a", 1]
