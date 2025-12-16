134c9244591e7842e8886790903dae1cdab68a4e
dskim6797 <dskim6797@gmail.com>
2025-12-16 17:41:23 +0900

    refactor: Restructure Django day02 practice folder

---
+++ practices/11_Django/02_bookmark/bookmark/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from bookmark.models import Bookmark
+
+
+def bookmark_list(request):
+    # bookmarks = Bookmark.objects.all() # SELECT * FROM bookmark_bookmark
+    bookmarks = Bookmark.objects.filter(id__gte=50)
+    context = {
+        'bookmarks': bookmarks
+    }
+
+    # return HttpResponse("<h1>북마크 리스트 페이지 입니다</h1>")
+    return render(request, 'bookmark_list.html', context)
+
+def bookmark_detail(request, pk):
+    # try:
+    #     bookmark = Bookmark.objects.get(pk=pk)
+    # except:
+    #     raise Http404
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+
+    context = {'bookmark':bookmark}
+    return render(request, 'bookmark_detail.html', context)
